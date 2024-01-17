45e9bf0511da4a33ca0a2c83a71d3b428ee8d2be
echeverriaseba <secheverriap@gmail.com>
2024-01-17 11:41:01 +0100

    Initial setup

---
+++ src/Spohia_PCA.R
@@ -0,0 +1,6 @@
+
+############# PCA Tests - Sophia's Data ##############
+
+Sites <- read.csv("data/1_Sites.csv", fileEncoding="latin1", na.strings=c("","NA"))
+Nutrients <- read.csv("data/1_Nutrients.csv", fileEncoding="latin1", na.strings=c("","NA"))
+
